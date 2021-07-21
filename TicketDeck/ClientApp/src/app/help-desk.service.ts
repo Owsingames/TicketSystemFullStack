0942eac4450435cee1a543fc18f6c29578a57c5f
tmvizina <tmvizina@gmail.com>
2021-07-21 09:28:50 -0400

    added routing for user component to nav bar

---
+++ TicketDeck/ClientApp/src/app/help-desk.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+
+
+export class User {
+  userID: number;
+  Name: string;
+}
+
+
+export class HelpDeskService {
+  constructor(private http: HttpClient) {
+
+  }
+  //Not super confident with this subscribe method really
+  controller: string = '';
+  baseURL: string = `https://localhost:44314/api/${this.controller}`
+
+  getUser(): any {
+    this.controller='Users'
+    return this.http.get<User>(this.baseURL);
+  }
+
+  //getTickets(): any {
+  //  this.controller = 'Tickets'
+  //  return this.http.get<Ticket>(this.baseURL);
+  //}
+
+  //getBookmarks(): any {
+  //  this.controller = 'Bookmarks'
+  //  return this.http.get<Bookmark>(this.baseURL);
+  //}
+}
